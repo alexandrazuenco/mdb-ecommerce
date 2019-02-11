7f74ec4d29968713653434add7fe97b9b98d31c9
alexandrazuenco <alexandra.zuenco@gmail.com>
2019-02-11 19:51:38 +0200

    project

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// jQuery
+import 'jquery';
+
+// PopperJS
+import 'popper.js';
+
+// Bootstrap 4
+import 'bootstrap';
+
+// polifill to picture tag
+import 'picturefill';
+
+// Material Design Bootstrap
+import '../vendors/mdb/js/mdb';
+
+// Fontawesome 5
+import './fa';
+
+import './components/pdpViewer';
+
+import './product-list';
